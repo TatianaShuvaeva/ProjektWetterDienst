8f8c0a6a2267b0038ac3ac66c586d899da75a019
TatianaShuvaeva <talisman1982@gmail.com>
2024-11-21 15:52:51 +0100

    Code in zwei Funktionen aufgeteilt

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,34 @@
-document.getElementById('getWeatherBtn').addEventListener('click', function () {
-    const city = document.getElementById('city').value;
-    if (!city) {
+document.getElementById('getWeatherBtn').addEventListener('click', wetterAbrufen);
+
+function wetterAbrufen() {
+    const stadt = document.getElementById('city').value;
+    if (!stadt) {
         alert('Bitte geben Sie eine Stadt ein');
         return;
     }
 
-    fetch(`/wetter?stadt=${city}`)
+    fetch(`/wetter?stadt=${stadt}`)
         .then(response => response.json())
-        .then(data => {
-            if (data.error) {
-                document.getElementById('weatherInfo').innerHTML = 'Stadt nicht gefunden!';
-            } else {
-                document.getElementById('weatherInfo').innerHTML = `
-                            <p>Temperatur: ${data.temperature}°C</p>
-                            <p>Minimale Temperatur: ${data.min_temperature}°C</p>
-                            <p>Maximale Temperatur: ${data.max_temperature}°C</p>
-                            <p>Windgeschwindigkeit: ${data.wind} m/s</p>
-                            <p>Luftfeuchtigkeit: ${data.luftfeuchtigkeit}%</p>
-                            <p>Sonnenaufgang: ${new Date(data.sonnenaufgang).toLocaleTimeString()}</p>
-                            <p>Sonnenuntergang: ${new Date(data.sonnenuntergang).toLocaleTimeString()}</p>
-                        `;
-            }
+        .then(daten => {
+            wetterAnzeigen(daten);
         })
-        .catch(error => {
+        .catch(fehler => {
             document.getElementById('weatherInfo').innerHTML = 'Fehler beim Abrufen der Daten';
         });
-});
+}
+
+function wetterAnzeigen(daten) {
+    if (daten.error) {
+        document.getElementById('weatherInfo').innerHTML = 'Stadt nicht gefunden!';
+    } else {
+        document.getElementById('weatherInfo').innerHTML = `
+            <p>Temperatur: ${daten.temperature}°C</p>
+            <p>Minimale Temperatur: ${daten.min_temperature}°C</p>
+            <p>Maximale Temperatur: ${daten.max_temperature}°C</p>
+            <p>Windgeschwindigkeit: ${daten.wind} m/s</p>
+            <p>Luftfeuchtigkeit: ${daten.luftfeuchtigkeit}%</p>
+            <p>Sonnenaufgang: ${new Date(daten.sonnenaufgang).toLocaleTimeString()}</p>
+            <p>Sonnenuntergang: ${new Date(daten.sonnenuntergang).toLocaleTimeString()}</p>
+        `;
+    }
+}
