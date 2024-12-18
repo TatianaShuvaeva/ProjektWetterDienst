84367d9a05395987122382f194b9ef9b834c09a9
Arkadiy Shuvaev <a.shuvaev@ev-digitalinvest.de>
2024-12-18 11:49:16 +0000

    removed installation of Cypress

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -10,13 +10,6 @@ source .venv/bin/activate
 # Install Python packages
 pip install -r requirements.txt
 
-# Install cypress
-apt-get update
-apt-get -y install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
-npx cypress install
-
-# export DISPLAY=host.docker.internal:0.0
-
 # Access control disabled, clients can connect from any host
 xhost +
 
