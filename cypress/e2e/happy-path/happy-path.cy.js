f38105840815a91968fee860c2e0770754cce758
TatianaShuvaeva <talisman1982@gmail.com>
2025-01-02 20:04:38 +0000

    Happy Path verbessert

--- cypress/e2e/happy-path/happy-path.cy.js
+++ cypress/e2e/happy-path/happy-path.cy.js
@@ -5,9 +5,10 @@ describe('Happy Path', () => {
 
 
     it('das Wetter für die Stadt anzeigt', () => {
-        cy.wait(5000);
-        cy.get('[data-cy="stadt-eingeben"]').type('Potsdam');
-        cy.pause();
+        cy.wait(2000);
+        cy.get('[data-cy="stadt-eingeben"]').type('München');
+
+        cy.wait(2000);
         cy.get('[data-cy="wetter-abfragen" ]').click();
 
         cy.get('[data-cy="temperatur"]').should('not.be.empty');
@@ -17,10 +18,31 @@ describe('Happy Path', () => {
         cy.get('[data-cy="luftfeuchtigkeit"]').should('not.be.empty');
         cy.get('[data-cy="sonnenaufgang"]').should('not.be.empty');
         cy.get('[data-cy="sonnenuntergang"]').should('not.be.empty');
+
+        cy.wait(2000);
+        cy.get('#weatherChart').scrollIntoView();
+        cy.get('#weatherChart').should('be.visible');
     });
 
+    it('das Wetter für die Stadt mit umlaut anzeigt', () => {
+        cy.wait(2000);
+        cy.get('[data-cy="stadt-eingeben"]').type('Muenchen');
 
+        cy.wait(2000);
+        cy.get('[data-cy="wetter-abfragen" ]').click();
 
+        cy.get('[data-cy="temperatur"]').should('not.be.empty');
+        cy.get('[data-cy="min-temperatur"]').should('not.be.empty');
+        cy.get('[data-cy="max-temperatur"]').should('not.be.empty');
+        cy.get('[data-cy="windgeschwindigkeit"]').should('not.be.empty');
+        cy.get('[data-cy="luftfeuchtigkeit"]').should('not.be.empty');
+        cy.get('[data-cy="sonnenaufgang"]').should('not.be.empty');
+        cy.get('[data-cy="sonnenuntergang"]').should('not.be.empty');
+
+        cy.wait(2000);
+        cy.get('#weatherChart').scrollIntoView();
+        cy.get('#weatherChart').should('be.visible');
+    });
 })
 
 
